39b16032a0f3c3f8ee057e578c10cfd39c2010cd
仝曦文 <binzeefox@outlook.com>
2020-07-27 10:33:33 +0800

    FoxActivity添加全屏方法 fullScreen()

--- FoxFrame/src/main/java/com/binzeefox/foxframe/core/base/FoxActivity.java
+++ FoxFrame/src/main/java/com/binzeefox/foxframe/core/base/FoxActivity.java
@@ -2,11 +2,15 @@ package com.binzeefox.foxframe.core.base;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
 import android.util.SparseBooleanArray;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.binzeefox.foxframe.core.FoxCore;
 import com.binzeefox.foxframe.core.base.callbacks.PermissionCallback;
@@ -29,8 +33,9 @@ import io.reactivex.functions.Consumer;
 
 /**
  * Activity基类
- *
+ * <p>
  * 分离了视图和onCreate
+ *
  * @author binze
  * 2019/12/10 11:48
  */
@@ -46,6 +51,7 @@ public abstract class FoxActivity extends AppCompatActivity {
 
     /**
      * 通过资源ID加载布局，优先级较低
+     *
      * @author binze 2019/12/10 12:07
      */
     protected int onSetLayoutResource() {
@@ -54,6 +60,7 @@ public abstract class FoxActivity extends AppCompatActivity {
 
     /**
      * 通过View加载布局，优先级较高
+     *
      * @author binze 2019/12/10 12:08
      */
     protected View onSetLayoutView() {
@@ -62,16 +69,18 @@ public abstract class FoxActivity extends AppCompatActivity {
 
     /**
      * 代理onCreate 业务部分
+     *
      * @author binze 2019/12/10 12:09
      */
     protected abstract void create(Bundle savedInstanceState);
 
     /**
      * 请求权限
+     *
      * @author binze 2019/12/10 12:11
      */
     protected void requestPermission(List<String> permissionList
-            , final PermissionCallback callback){
+            , final PermissionCallback callback) {
         PermissionUtil.get(permissionList).request(this)
                 .subscribe(new Observer<List<String>>() {
                     @Override
@@ -97,42 +106,61 @@ public abstract class FoxActivity extends AppCompatActivity {
     }
 
     /**
+     * 全屏
+     *
+     * @author binze 2020/7/27 10:32
+     */
+    public void fullScreen() {
+        Window window = getWindow();
+        View decorView = window.getDecorView();
+        int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        decorView.setSystemUiVisibility(option);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+    }
+
+    /**
      * 跳转
+     *
      * @author binze 2019/12/10 12:37
      */
-    public Navigator navigate(Class<? extends Activity> target){
+    public Navigator navigate(Class<? extends Activity> target) {
         return new Navigator(this, target);
     }
 
     /**
      * 隐式跳转
+     *
      * @author binze 2019/12/10 12:54
      */
-    public Navigator navigate(Intent intent){
+    public Navigator navigate(Intent intent) {
         return new Navigator(this, intent);
     }
 
     /**
      * 隐式参数跳转
+     *
      * @author binze 2019/12/11 11:31
      */
-    public Observable<ActivityRequestUtil.Result> requestActivity(Intent intent, Bundle options){
+    public Observable<ActivityRequestUtil.Result> requestActivity(Intent intent, Bundle options) {
         return ActivityRequestUtil.init(intent, options).request(getSupportFragmentManager());
     }
 
     /**
      * 从上一界面获取参数
+     *
      * @author binze 2019/12/10 12:58
      */
-    public Bundle getDataFromNavigate(){
+    public Bundle getDataFromNavigate() {
         return Navigator.getDataFromNavigate(getIntent());
     }
 
     /**
      * 获取视图帮助类
+     *
      * @author binze 2019/12/10 13:09
      */
-    public ViewHelper getViewHelper(){
+    public ViewHelper getViewHelper() {
         return new ViewHelper() {
             @Override
             public <T extends View> T findView(int id) {
@@ -144,15 +172,15 @@ public abstract class FoxActivity extends AppCompatActivity {
     /**
      * 判断该方法是否在超时前被调用两次
      *
-     * @param timeout   超时时间(ms)
-     * @param id    检测id，用于区分事件
+     * @param timeout 超时时间(ms)
+     * @param id      检测id，用于区分事件
      * @return 若调在超时前调用该方法两次，则返回true
      */
-    protected boolean checkCallAgain(long timeout, final int id){
+    protected boolean checkCallAgain(long timeout, final int id) {
         Disposable timer = mTimerQueue.get(id);
         boolean flag = mFlagQueue.get(id);
 
-        if (timer != null && !timer.isDisposed() && flag){
+        if (timer != null && !timer.isDisposed() && flag) {
             //在倒计时并且已经点过
             mFlagQueue.delete(id);
             mTimerQueue.delete(id);
@@ -174,12 +202,14 @@ public abstract class FoxActivity extends AppCompatActivity {
         mTimerQueue.put(id, timer);
         return false;
     }
-    protected boolean checkCallAgain(int timeout){
+
+    protected boolean checkCallAgain(int timeout) {
         return checkCallAgain(timeout, -1); //默认id-1
     }
 
     /**
      * 生命周期 onCreate
+     *
      * @author binze 2019/12/10 13:06
      */
     @Override
@@ -200,6 +230,7 @@ public abstract class FoxActivity extends AppCompatActivity {
 
     /**
      * 生命周期 onResume
+     *
      * @author binze 2020/6/29 15:50
      */
     @Override
@@ -213,17 +244,18 @@ public abstract class FoxActivity extends AppCompatActivity {
 
     /**
      * 生命周期 onDestroy
+     *
      * @author binze 2019/12/10 13:06
      */
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (core != null) core.removeActivity(this);
-        if (dContainer != null && !dContainer.isDisposed()){
+        if (dContainer != null && !dContainer.isDisposed()) {
             dContainer.dispose();
             dContainer.clear();
         }
-        if (dContainer3 != null && !dContainer3.isDisposed()){
+        if (dContainer3 != null && !dContainer3.isDisposed()) {
             dContainer3.dispose();
             dContainer3.clear();
         }
