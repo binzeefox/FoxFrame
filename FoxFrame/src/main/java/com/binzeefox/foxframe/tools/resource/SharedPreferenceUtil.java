c0c74890434306155e517dcf266ca4a020a9200f
仝曦文 <binzeefox@outlook.com>
2020-08-25 12:09:44 +0800

    修正SharedPreferenceUtil部分问题

--- FoxFrame/src/main/java/com/binzeefox/foxframe/tools/resource/SharedPreferenceUtil.java
+++ FoxFrame/src/main/java/com/binzeefox/foxframe/tools/resource/SharedPreferenceUtil.java
@@ -16,8 +16,6 @@ import androidx.annotation.NonNull;
  * 2019/9/29 10:30
  */
 public class SharedPreferenceUtil {
-    public static final String FILENAME_CONFIG = "config";  //预设的配置文件 文件名
-    public static final String CONFIG_LAST_USER = "last_login"; //预设的上次登录用户字段名
 
     @SuppressLint("StaticFieldLeak")
     private static SharedPreferenceUtil sInstance;
@@ -66,12 +64,12 @@ public class SharedPreferenceUtil {
      * 读取字符串数据
      * @param fileName 文件名
      * @param key   键
-     * @param defaultKey    默认返回值
+     * @param defaultValue    默认返回值
      * @return  值
      */
-    public synchronized String readString(String fileName, String key, String defaultKey){
+    public synchronized String readString(String fileName, String key, String defaultValue){
         SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
-        return sp.getString(key, defaultKey);
+        return sp.getString(key, defaultValue);
     }
 
     /**
@@ -89,38 +87,28 @@ public class SharedPreferenceUtil {
     }
 
     /**
-     * 读取Config信息
+     * 读取字符串数据
+     * @param fileName 文件名
      * @param key   键
-     * @return  信息
+     * @param defaultValue    默认返回值
+     * @return  值
      */
-    public synchronized String readConfig(String key){
-        return readString(FILENAME_CONFIG, key, "");
+    public synchronized int readInteger(String fileName, String key, int defaultValue){
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        return sp.getInt(key, defaultValue);
     }
 
     /**
-     * 写入Config信息
+     * 写入字符串数据
+     * @param fileName  文件名
      * @param key   键
-     * @param value  信息
+     * @param value 值
      */
-    public synchronized void writeConfig(String key, String value){
-        writeString(FILENAME_CONFIG, key, value);
-    }
+    public synchronized void writeInteger(String fileName, String key, int value){
+        SharedPreferences.Editor editor =
+                mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE).edit();
 
-    /**
-     * 示例代码：写入最后登录用户名
-     * @author binze 2019/11/19 9:40
-     */
-    public void saveLastLoginUser(String username){
-        writeConfig(CONFIG_LAST_USER, username);
-    }
-
-    /**
-     * 示例代码：读取最后登录用户名
-     *
-     * @return 最后登录的用户名，若不存在则返回 ""
-     * @author binze 2019/11/19 9:41
-     */
-    public String getLastLoginUser(){
-        return readConfig(CONFIG_LAST_USER);
+        editor.putInt(key, value);
+        editor.apply();
     }
 }
